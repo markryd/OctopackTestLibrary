3ebae9dccdd4866a9a14e19fc19c8f6c90779a53
Mark Rydstrom <mark.rydstrom@octopus.com>
2017-01-23 12:19:36 +1000

    Add project files.

---
+++ OctopackTestLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OctopackTestLibrary
+{
+    public class Class1
+    {
+    }
+}
